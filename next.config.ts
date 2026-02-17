3c930d505a2d9961a8d947dc1748925c67315bbb
darindrobinski <darin.drobinski@improving.com>
2026-02-17 16:18:20 -0600

    checkpoint: before chunk 3 -- production-config-tweaks

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  serverExternalPackages: ['sharp', 'onnxruntime-node'],
-  outputFileTracingExcludes: {
-    '/': [
-      'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/!(libonnxruntime.so.1|onnxruntime_binding.node)',
-    ],
-  },
 };
 
 export default nextConfig;
